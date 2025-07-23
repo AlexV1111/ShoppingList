2a69cecc124ec201c1f7867b70602ba7aa388c34
Alex <Alexvvv9511@yandex.ru>
2025-07-23 14:01:42 +0300

    make presentation layer

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.shoppinglist.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.media3.common.util.Log
 import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this){
+            Log.d("MainActivityTest", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
